af99f88db2bceddf3cb519b0f3eb3743ff47a943
Bohdan Konorin <bohdank@titan.io>
2026-01-15 14:58:45 +0100

    Added footer

--- ui/src/components/Layouts/DefaultLayout.tsx
+++ ui/src/components/Layouts/DefaultLayout.tsx
@@ -1,36 +1,33 @@
 import styled from "@mui/material/styles/styled";
 import Box from "@mui/material/Box";
 import type { FC, PropsWithChildren } from "react";
-import { useModal } from "../../hooks/useModal";
 import { Header } from "../Header";
-import { ResponsiveNavigation } from "../Navigation/Navigation";
+import { Footer } from "../Footer";
 import { useMediaQuery } from "@mui/material";
 
 type Props = PropsWithChildren<{ pageTitle: string }>;
 
 export const DefaultLayout: FC<Props> = ({ children, pageTitle }) => {
-  const sidebar = useModal();
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <BodyWrapper>
-      <Box component="nav" sx={{ width: { md: drawerWidth }, flexShrink: { sm: 0 } }}>
-        <ResponsiveNavigation sidebarOpen={sidebar.isOpen} setSidebarOpen={sidebar.setOpen} drawerWidth={drawerWidth} />
-      </Box>
       <Box
         sx={{
-          marginLeft: "auto",
           flexGrow: 1,
           p: isMobile ? 2 : 3,
-          width: { xs: "100%", sm: "100%", md: `calc(100% - ${drawerWidth}px)` },
+          width: "100%",
           minHeight: "100vh",
           color: "white",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
         <ContentWrapper>
-          <Header setSidebarOpen={sidebar.setOpen} pageTitle={pageTitle} />
+          <Header pageTitle={pageTitle} />
           <Box component="main">{children}</Box>
         </ContentWrapper>
+        <Footer />
       </Box>
     </BodyWrapper>
   );
@@ -44,6 +41,5 @@ const ContentWrapper = styled("div")`
   max-width: 1920px;
   margin: 0 auto;
   width: 100%;
+  flex-grow: 1;
 `;
-
-const drawerWidth = 240;
